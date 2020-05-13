16f73a0cceee0857868c82ee7a1eaea28a438eca
Angel Suarez Perez <angelsupe5@gmail.com>
2020-05-13 18:25:43 +0200

    commit11

--- Game.java
+++ Game.java
@@ -125,6 +125,9 @@ public class Game
         else if (commandWord.equals("look")) {
             look();
         }
+         else if (commandWord.equals("eat")) {
+            eat();
+        }
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
@@ -201,4 +204,7 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
+    private void eat() {
+        System.out.println("You have eaten now and you are not hungry any more");
+    }
 }
